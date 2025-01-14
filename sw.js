14575b3f61b834f247ca7064a0ef2d7267e56ab0
FlukieL <72304964+FlukieL@users.noreply.github.com>
2025-01-14 11:16:31 +0000

    Added manifest and icons

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'luke-links-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/script.js',
+  '/assets/imagens/profile.jpg',
+  '/assets/imagens/favicon.ico',
+  '/fa6.6/css/all.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+}); 
